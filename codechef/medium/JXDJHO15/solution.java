cf98eb1f3f9b398bbc5844415c53e8bcc600e937
MaryJones2412 <m.maryjones2412@gmail.com>
2026-08-17 19:27:06 +0530

    ✅ JXDJHO15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/JXDJHO15/solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Book {
+    String title;
+    String author;
+    int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[" +
+                "title=" + title +
+                ", author=" + author +
+                ", year=" + year +
+                "]";
+    }
+
+}
+
+class Codechef {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        String title = scanner.nextLine();
+        
+        String author = scanner.nextLine();
+        
+        int year = scanner.nextInt();
+
+        Book book1 = new Book(title, author, year);
+
+        System.out.println(book1); 
+
+        scanner.close();
+    }
+}
