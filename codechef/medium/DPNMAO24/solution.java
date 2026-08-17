eba20cc9beec546bddbd282dc72d2dd2a7207f90
MaryJones2412 <m.maryjones2412@gmail.com>
2026-08-17 16:21:41 +0530

    ✅ DPNMAO24 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/medium/DPNMAO24/solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        List<Integer> uniqueElements = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+
+        for (int x : arr) {
+            if (!seen.contains(x)) {
+                uniqueElements.add(x);
+                seen.add(x);
+            }
+        }
+
+        for (int i = 0; i < uniqueElements.size(); i++) {
+            System.out.print(uniqueElements.get(i));
+            if (i != uniqueElements.size() - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        sc.close();
+    }
+}
