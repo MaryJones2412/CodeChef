0ebef48b647a2143584e05b19cb3818479f82922
MaryJones2412 <m.maryjones2412@gmail.com>
2026-08-18 19:53:24 +0530

    ✅ BXCERA14 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/BXCERA14/solution.cpp
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class FavoriteSongsList {
+    private ArrayList<String> songs;
+
+    public FavoriteSongsList() {
+        // Initialize the list of songs
+        songs = new ArrayList<>();
+    }
+
+    public void addSong(String song) {
+        songs.add(song);
+    }
+
+    public void saveSongs() {
+        try {
+            File file = new File("songs.txt");
+            FileWriter writer = new FileWriter(file);
+            for (String song : songs) {
+                writer.write(song + System.lineSeparator());
+            }
+            writer.close();
+            System.out.println("Songs saved to songs.txt");
+        } catch (Exception e) {
+            System.out.println("An error occurred while saving songs.");
+        }
+    }
+}
+
+class Codechef {
+    public static void main(String[] args) {
+        FavoriteSongsList myMusic = new FavoriteSongsList();
+        myMusic.addSong("Bohemian Rhapsody");
+        myMusic.addSong("Imagine");
+
+        // Save the songs to the file
+        myMusic.saveSongs();
+    }
+}
+import java.io.FileWriter;
+import java.io.File;
+import java.util.ArrayList;
