b93eaffbfcee792a532315b54de4c1f64cf31998
MaryJones2412 <m.maryjones2412@gmail.com>
2026-08-20 20:45:10 +0530

    ✅ COPS - Rating 1242 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-20

--- codechef/easy/COPS---Rating-1242/solution.java
+++ codechef/easy/COPS---Rating-1242/solution.java
@@ -1,29 +1,23 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
 class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
+{public static void main (String[] args) throws java.lang.Exception
 	{
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
-        
         while (t-- > 0) {
             int M = scanner.nextInt();
             int x = scanner.nextInt();
             int y = scanner.nextInt();
-
             int[] copHouses = new int[M];
             for (int i = 0; i < M; i++) {
                 copHouses[i] = scanner.nextInt();
             }
-
             boolean[] safeHouses = new boolean[100]; 
             for (int i = 0; i < 100; i++) {
                 safeHouses[i] = true; 
             }
-
             for (int copHouse : copHouses) {
                 int startHouse = Math.max(1, copHouse - x * y);
                 int endHouse = Math.min(100, copHouse + x * y);
@@ -31,17 +25,14 @@ class Codechef
                     safeHouses[j - 1] = false; 
                 }
             }
-
             int countSafeHouses = 0;
             for (boolean isSafe : safeHouses) {
                 if (isSafe) {
                     countSafeHouses++;
                 }
             }
-
             System.out.println(countSafeHouses);
         }
-        
         scanner.close();
     }
 }
