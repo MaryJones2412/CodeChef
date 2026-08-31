b59c1d5a48a3fb739c4973ab3837bbe6d10c3272
MaryJones2412 <m.maryjones2412@gmail.com>
2026-08-31 12:49:32 +0530

    ✅ VBHXB05 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/VBHXB05/solution.cpp
@@ -0,0 +1,21 @@
+console.log(currentHour);
+
+// complete your code from here
+{
+    greeting="Good Afternoon!";
+}
+else
+{
+    greeting="Good Evening!";
+}
+console.log(greeting);
+
+else if(currentHour>=12 && currentHour<=18)
+}
+    greeting = "Good Morning!";
+
+if (currentHour < 12) {
+
+let greeting;
+const currentHour = new Intl.DateTimeFormat('en-US', options).format(new Date());
+const options = { timeZone: 'Asia/Kolkata', hour: 'numeric', hour12: false };
