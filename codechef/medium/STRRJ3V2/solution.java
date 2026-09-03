2197309be9b0b3b8c52f6d1c81d3d0f774666945
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-03 05:43:26 +0530

    ✅ STRRJ3V2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/medium/STRRJ3V2/solution.java
@@ -0,0 +1,12 @@
+//We need to output 2569 using string concatenation.
+//The code below is incorrect - Debug the code to solve the problem
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String x= "25";
+        String y= "69";
+        System.out.print(x + y);
+	}
+}
