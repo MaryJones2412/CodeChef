cd2fe56737ddad2f3404471e5af435d956010b70
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-07 07:27:53 +0530

    ✅ VBHXB16 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/VBHXB16/solution.cpp
@@ -0,0 +1,14 @@
+
+        });
+        console.log("Error:", error);
+        })
+        .catch((error) => {
+        console.log(iceCreamWithToppings);
+        .then((iceCreamWithToppings) => {
+        })
+        return addToppings(frozenIceCream);
+        console.log(frozenIceCream);
+        })
+        .then((frozenIceCream) => {
+        console.log(mixedIngredients);
+        return freezeMixture(mixedIngredients);
