b0e50dc8a2349562804bdfa572bc888b8e0993a8
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-07 07:10:00 +0530

    ✅ POLMDB66 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/POLMDB66/solution.cpp
@@ -0,0 +1,14 @@
+// Comment Schema
+
+.catch(err => console.error("❌ MongoDB connection error:", err));
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+})
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+mongoose.connect(uri, {
+// Connect to MongoDB Atlas
+
+const uri="mongodb+srv://mmaryjones2412_db_user:M.Mary2412@cluster0.efjh5pi.mongodb.net/?appName=Cluster0";
+// MongoDB Atlas Connection String
+
+const mongoose = require('mongoose');
