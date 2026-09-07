43f97357ac5ba3f0710ee8560129b086717adbc0
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-07 06:18:02 +0530

    ✅ LJAAS50 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/LJAAS50/solution.java
+++ codechef/medium/LJAAS50/solution.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 class Codechef
 {
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        double radius = scanner.nextDouble();
-        double area = 3.14 * radius * radius;
-        System.out.println(area);
+        String one = scanner.nextLine();
+        String two = scanner.nextLine();
+        System.out.println(one + " " + two);
     }
 }
