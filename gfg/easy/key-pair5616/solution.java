c7083e75cb258ba6705fbc814af4dec25e598d40
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-10 19:51:58 +0530

    ✅ Two Sum - Pair with Given Sum | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-10

---
+++ gfg/easy/key-pair5616/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(target - arr[i])) {
+                return true;
+            }
+            set.add(arr[i]);
+        }
+        return false;
+    }
+}
