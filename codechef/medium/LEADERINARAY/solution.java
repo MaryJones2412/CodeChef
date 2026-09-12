484ef48a6d1b15cf5b1f1505cb0bf52edc656f2e
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-12 19:59:04 +0530

    ✅ LEADERINARAY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-12

--- codechef/medium/LEADERINARAY/solution.java
+++ codechef/medium/LEADERINARAY/solution.java
@@ -1,23 +1,18 @@
 class Solution {
-    public List<Integer> findLeaders(int[] nums) {
-        int n = nums.length;
-        List<Integer> leaders = new ArrayList<>();
-        int maxFromRight = nums[n - 1];
+    public List<Integer> findLeaders(int[] a) {
+        int n = a.length;
+        List<Integer> l = new ArrayList<>();
+        int m = a[n - 1];
+        l.add(m);
 
-        // Last element is always a leader
-        leaders.add(nums[n - 1]);
-
-        // Traverse from right to left
         for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] > maxFromRight) {
-                leaders.add(nums[i]);
-                maxFromRight = nums[i];
+            if (a[i] > m) {
+                l.add(a[i]);
+                m = a[i];
             }
         }
 
-        // Reverse list to maintain original order
-        Collections.reverse(leaders);
-        return leaders;
+        Collections.reverse(l);
+        return l;
     }
 }
-
