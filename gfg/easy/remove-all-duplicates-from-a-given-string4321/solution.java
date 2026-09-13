937cd8cea70f1ce2ec0bada10b17091b8341c137
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-13 20:30:00 +0530

    ✅ String Duplicates Removal | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ gfg/easy/remove-all-duplicates-from-a-given-string4321/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    String removeDuplicates(String s) {
+        boolean[] seen = new boolean[128];
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!seen[ch]) {
+                result.append(ch);
+                seen[ch] = true;
+            }
+        }
+
+        return result.toString();
+    }
+}
