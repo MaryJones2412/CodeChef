f20bf37989959db18685ad3604a7549916b8fabc
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-14 10:26:30 +0530

    ✅ DSCPPAS263 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/medium/DSCPPAS263/solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        if ((n * m) % 2 != 0) {
+            System.out.println(-1);
+        } else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    System.out.print(1 + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        scanner.close();
+    }
+}
