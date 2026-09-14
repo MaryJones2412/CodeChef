d7a7f8c3e92a4ee7e02196730424f76c0b0619eb
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-14 10:14:47 +0530

    ✅ MATTREVERSE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/medium/MATTREVERSE/solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] mat = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
