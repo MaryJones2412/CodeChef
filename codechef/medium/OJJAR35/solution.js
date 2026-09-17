d4a209e62615dc5e9de780283e9c28aee0bcddae
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-17 16:17:58 +0530

    ✅ OJJAR35 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/OJJAR35/solution.js
+++ codechef/medium/OJJAR35/solution.js
@@ -1,6 +1,6 @@
-const words = ['hello', 'world', 'javascript'];
+const numbers = [1, 2, 3, 4, 5];
 
 // complete the code 
-const uppercaseWords=words.map(word=>word.toUpperCase()) ;
+const squaredNumbers = numbers.map(/*write you code here*/num=>num*num); 
 
-console.log(uppercaseWords); 
+console.log(squaredNumbers); 
