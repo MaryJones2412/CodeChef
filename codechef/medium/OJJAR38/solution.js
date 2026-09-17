c27674a809d8b4a49616e98822ded2a79f802c5f
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-17 16:19:59 +0530

    ✅ OJJAR38 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/medium/OJJAR38/solution.js
@@ -0,0 +1,11 @@
+let scores = [10, 45, 23, 8];
+let totalScore = 0; 
+
+// complete the code 
+scores.forEach(score=>{totalScore+=score;});
+// Use forEach to go through each score
+
+// Add the current score to the totalScore
+  
+
+console.log("Total Score:", totalScore); 
