7e7b78a6a96a91814f8cdadcc08fabaa4ea2b8af
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-19 06:09:31 +0530

    ✅ OJJAR43 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-19

--- codechef/medium/OJJAR43/solution.js
+++ codechef/medium/OJJAR43/solution.js
@@ -1,7 +1,8 @@
-let numbers = [10, -5, 0, 3, -2, 7, -1, 4];
+let words = ["apple", "banana", "pear", "kiwi", "grape", "orange"];
 
 // complete the code 
-let positiveNumbers=numbers.filter(number => {
-    return number >=0;
+let shortWords=words.filter(word => {
+    return word.length <=4;
+
 });
-console.log(positiveNumbers);
+console.log(shortWords);
