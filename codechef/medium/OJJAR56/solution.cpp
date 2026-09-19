41b3402ea7a891535b3f39877860da47d40560dd
MaryJones2412 <m.maryjones2412@gmail.com>
2026-09-19 06:26:17 +0530

    ✅ OJJAR56 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-19

---
+++ codechef/medium/OJJAR56/solution.cpp
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+  <div
+  style={{
+  width: "200px",
+  height: "100px",
+  backgroundColor: 'lightblue',
+  color: 'white',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '18px'
+  }}
+  >
+  Styled Box
+  </div>
+  );
+  }
